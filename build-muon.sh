ccc8c486f9f2eaef0e0f9db7fa2be9b1dad1376d
cvicens <carlos.vicens.alonso@gmail.com>
2022-03-23 17:26:08 +0000

    quick and dirty but working example

--- build-muon.sh
+++ build-muon.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 FEDORA_VERSION=35
-sudo podman build --build-arg FEDORA_VERSION=${FEDORA_VERSION} --tag muon:${FEDORA_VERSION} -f Containerfile .
+sudo podman build --build-arg FEDORA_VERSION=${FEDORA_VERSION} --tag muon:${FEDORA_VERSION} -f Containerfile.muon .
